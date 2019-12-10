55b433175f4b93dea81d08c8bba980c3b0387e70
Jean-jacques Acens <jacens@z1r5p4.le-101.fr>
2019-12-10 18:59:49 +0100

    change order

--- srcs/script.sh
+++ srcs/script.sh
@@ -8,13 +8,13 @@ service php7.3-fpm start
 
 #--- CONFIG MYSQL (BDD) ---#
 #            WP            #
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mysql -u root -e "CREATE USER 'utilisateur'@'%' IDENTIFIED BY 'mot de passe';"
 mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'utilisateur'@'%' WITH GRANT OPTION;"
 mysql -u root -e "FLUSH PRIVILEGES;"
 #---					---#
 
 #           USER           #
-mysql -u root -e "CREATE DATABASE IF NOT EXISTS wordpress;"
 mysql -u root -e "CREATE USER '$USER_WORDPRESS'@'localhost' identified by '$PASSWORD';" 
 mysql -u root -e "CREATE DATABASE wordpress;" 
 mysql -u root -e "GRANT ALL PRIVILEGES ON wordpress.* TO '$USER_WORDPRESS'@'localhost';" 
